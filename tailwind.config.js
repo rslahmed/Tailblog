0b89a64a27cd44e70cf21888fbbf6c7b44363a1f
Rasel Ahmed <rsl.ahmed383@gmail.com>
2021-04-06 02:25:31 +0600

    files added

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily:{
+        'poppins': ['Poppins', 'sans-serif'],
+        'roboto': ['Roboto', 'sans-serif']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
